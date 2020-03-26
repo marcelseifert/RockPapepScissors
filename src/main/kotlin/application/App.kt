aef4b00e8152eb57686753145ee374ce48388640
Marcel <marcel.seifert@googlemail.com>
2020-03-26 17:43:54 +0100

    add application plugin so it can run from gradlew...set default rounds from 1 to 100

--- src/main/kotlin/application/App.kt
+++ src/main/kotlin/application/App.kt
@@ -5,13 +5,13 @@ import model.gamingstrategy.RandomGamingStrategy
 import model.rule.GameEngine
 
 fun main(args: Array<String>) {
-    var rounds = 1
+    var rounds = 100
 
     if (args.isEmpty() || args[0].toInt() <= 0) {
-        println("start RockPaperScissors with 1 round")
+        println("start RockPaperScissors with 100 rounds")
     } else {
         rounds = args[0].toInt()
-        println("start RockPaperScissors with ${args[0]} round")
+        println("start RockPaperScissors with ${args[0]} round(s)")
     }
 
     val playerOne = Player(RandomGamingStrategy(), "PlayerOne")
